7d5618df07ec428db8eac527ff46627184fdef4c
Olgar1an <baker.k.95@outlook.com>
2016-11-18 20:19:52 +0000

    APP
    Start file of the app

---
+++ BeaconWarning/app/src/main/java/com/example/kevin/beaconwarning/PhoneVibration.java
@@ -0,0 +1,9 @@
+package com.example.kevin.beaconwarning;
+import android.os.Vibrator;
+/**
+ * Created by kevin on 18/11/2016.
+ */
+
+public class PhoneVibration {
+    public void vibrate (100);
+}
