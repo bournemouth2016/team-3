558181eadbd4d33bf878df4d83064907e1272af0
Xyliea <annie@clickandhope.com>
2016-11-18 23:18:10 +0000

    Bluetooth Struggles
    Still trying

--- BlindBeacon/app/src/main/java/com/example/annie/blindbeacon/MainActivity.java
+++ BlindBeacon/app/src/main/java/com/example/annie/blindbeacon/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.annie.blindbeacon;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +21,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothAdapter mBtAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        if (mBluetoothAdapter == null) {
+        if (mBtAdapter == null) {
             //Device doesn't support bluetooth
         }
 
-        if (!mBluetoothAdapter.isEnabled()) {
+        if (!mBtAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        List<String> btDevices = new ArrayList<String>();
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+        //If there are paired devices
+
+
+            }
         }
     }
 }
