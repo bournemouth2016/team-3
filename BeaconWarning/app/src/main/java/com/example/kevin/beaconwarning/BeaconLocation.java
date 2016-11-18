c3b44c304d7cc175f0c673a05ca560699346e7cf
Olgar1an <baker.k.95@outlook.com>
2016-11-18 23:15:50 +0000

    APP

--- BeaconWarning/app/src/main/java/com/example/kevin/beaconwarning/BeaconLocation.java
+++ BeaconWarning/app/src/main/java/com/example/kevin/beaconwarning/BeaconLocation.java
@@ -1,16 +1,51 @@
 package com.example.kevin.beaconwarning;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
+/**
+ * Created by kevin on 18/11/2016.
+ */
 
-public class BeaconLocation extends AppCompatActivity {
-
+public class BeaconLocation extends Activity {
+    Vibrator mVibrator;
+    Button vib1;
+    TextView vib2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon_location);
+        // Find button 1
+        vib1 = (Button) findViewById(R.id.vibration);
+        vib2 = (TextView) findViewById(R.id.text1);
+        mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if(mVibrator.hasVibrator()) {
+            vib2.setText("vibyes");
+            Log.v("cCan Vibrate", "YES");
+        }
+        else {
+            vib2.setText("vibno");
+            Log.v("cCan Vibrate", "NO");
+        }
+        // Click Listener for button1
+        vib1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               // vib1.setText("vibyes");
+
+                // Vibrates for 300 Milliseconds
+                mVibrator.vibrate(3000);
+
+            }
+        });
     }
 
 }
--- BeaconWarning/app/src/main/java/com/example/kevin/beaconwarning/PhoneVibration.java
+++ BeaconWarning/app/src/main/java/com/example/kevin/beaconwarning/PhoneVibration.java
@@ -4,16 +4,21 @@ package com.example.kevin.beaconwarning;
         import android.os.Vibrator;
         import android.app.Activity;
         import android.content.Context;
+        import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
         import android.view.View;
         import android.view.View.OnClickListener;
         import android.widget.Button;
+        import android.widget.TextView;
+
 /**
  * Created by kevin on 18/11/2016.
  */
 
-public class PhoneVibration extends Activity {
+public class PhoneVibration extends AppCompatActivity {
     Vibrator mVibrator;
     Button vib1;
+    TextView vib2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,24 @@ public class PhoneVibration extends Activity {
         setContentView(R.layout.activity_beacon_location);
         // Find button 1
         vib1 = (Button) findViewById(R.id.vibration);
+        vib2 = (TextView) findViewById(R.id.text1);
         mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if(mVibrator.hasVibrator()) {
+            vib1.setText("vibyes");
+            Log.v("cCan Vibrate", "YES");
+        }
+        else {
+            vib1.setText("vibno");
+            Log.v("cCan Vibrate", "NO");
+        }
         // Click Listener for button1
        vib1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                vib1.setText("vibyes");
+
                // Vibrates for 300 Milliseconds
-               mVibrator.vibrate(300);
+               mVibrator.vibrate(3000);
+
                 }
             });
     }
