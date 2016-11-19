c46571ac2759dcdd820ace7cf5842ee054120e91
Olgar1an <baker.k.95@outlook.com>
2016-11-19 02:09:37 +0000

    User Interface
    User Interface Almost Complete

---
+++ CodeForGood4/app/src/main/java/com/example/christine/codeforgood4/Main3Activity.java
@@ -0,0 +1,33 @@
+package com.example.christine.codeforgood4;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        populateListView();
+    }
+
+    private void populateListView(){
+
+        // Create list of items
+        String rooms[] = {"Auditorium 1", "Auditorium 2", "Room 1", "Room 2", "Bathroom 1", "Bathroom 2"};
+        // Build adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item, rooms);
+        // Configure list view
+        ListView list = (ListView) findViewById(R.id.listViewMain);
+        list.setAdapter(adapter);
+    }
+
+}
